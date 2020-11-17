c6255e595b66d18cf6721ece756397f7914421a2
Christopher Butler <christopher.harold.butler@gmail.com>
2020-11-17 19:18:20 +0100

    initial commit

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,6 +1,7 @@
-import { mount } from 'products/ProductsIndex';
-import 'cart/CartShow';
+import { mount as productsMount } from 'products/ProductsIndex';
+import { mount as cartMount } from 'cart/CartShow';
 
 console.log('container');
 
-mount(document.querySelector('#dev-products'));
+productsMount(document.querySelector('#dev-products'));
+cartMount(document.querySelector('#dev-cart'));
